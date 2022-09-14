4ff6938869806d181f2103f3ece8589c562202f8
Azeez Ahmed <azeezgboladele@gmail.com>
2022-09-14 14:50:07 -0700

    Prnt_lowercase_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,13 +2,15 @@
 /**
  * print_alphabet_x10 - TO print alphabets ten times in lower case
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
+
 	int count = 0;
 	char k;
 
 	while (count++ <= 9)
 	{
+
 		for (k = 'a'; k <= 'z'; k++)
 		_putchar(k);
 		_putchar('\n');
