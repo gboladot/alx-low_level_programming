436febc8302981b6efa37a6cca691d4fb27d7cad
Azeez Ahmed <azeezgboladele@gmail.com>
2022-09-14 16:25:45 -0700

    F_prnt_abs_value_int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer.
+ * @k: The integer to be computed
+ * Return: absolute value of integer
+ */
+int _abs(int k)
+{
+	if (k >= 0)
+		return (k);
+	else
+		return (-k);
+}
