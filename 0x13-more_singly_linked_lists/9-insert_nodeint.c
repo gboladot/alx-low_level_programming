c7ee9a9ab4c012d970ed4904b5f7120f3589ff3e
Azeez Ahmed <azeezgboladele@gmail.com>
2022-10-25 22:03:13 -0700

    Insert_nodeint

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,15 +32,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	else if (!idx)
 	{
-		node->next = temp;
-		*head = node;
-		return (node);
+	node->next = temp;
+	*head = node;
+	return (node);
 	}
 
 	while (i < (idx - 1))
 	{
-		temp = temp->next;
-		i++;
+	temp = temp->next;
+	i++;
 	}
 	node->next = temp->next;
 	temp->next = node;
